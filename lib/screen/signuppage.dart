9d81798bf288cf71f1e0a7841b999bc506d3f4b1
Suhaibusman <suhaibusman54@gmail.com>
2023-10-11 12:29:41 -0700

    firebase practice

--- lib/screen/signuppage.dart
+++ lib/screen/signuppage.dart
@@ -87,93 +87,95 @@ class _SignUpScreenState extends State<SignUpScreen> {
   @override
 
   Widget build(BuildContext context) {
-    return Scaffold(
-       
-      body: Container(
-        
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-         decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("assets/images/signupscreenImage.png"),
-          fit: BoxFit.cover,
-        ),
-    
-        ),
-        child: Column(
-          children: [
-            const SizedBox(height: 50,),
-            SizedBox(
-              child: Column(
-              //  mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  
-                     const TextWidget(textMessage: "Sign up to Shh!", textColor: Colors.white, textSize: 40),
-                      
-                      const SizedBox(height: 40,),
-                       CustomButtonWidget(imageAddress: "assets/images/googlelogo.png", bgColor: Colors.black, textMessage: "Sign up with Google", textColor: Colors.white, textSize: 20, buttonWidth: MediaQuery.of(context).size.width*0.8,),
-                    const SizedBox(height: 40,),
-                     Image.asset("assets/images/continuewithEmail.png"),
-                      const SizedBox(height: 40,),
-                      CustomTextField(textFieldController: name, hintText: "Enter Your Name"),
-                      
-                  const SizedBox(height: 20,),
-                   CustomTextField(textFieldController: userName, hintText: "Enter Your Username"),
-                  const SizedBox(height: 20,),
-                      CustomTextField(textFieldController: emailAddress, hintText: "Enter Email"),
+    return SafeArea(
+      child: Scaffold(
+         
+        body: Container(
+          
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+           decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/signupscreenImage.png"),
+            fit: BoxFit.cover,
+          ),
+      
+          ),
+          child: Column(
+            children: [
+              // const SizedBox(height: 40,),
+              SizedBox(
+                child: Column(
+                //  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    
+                       const TextWidget(textMessage: "Sign up to Shh!", textColor: Colors.white, textSize: 40),
+                        
+                        const SizedBox(height: 20,),
+                         CustomButtonWidget(imageAddress: "assets/images/googlelogo.png", bgColor: Colors.black, textMessage: "Sign up with Google", textColor: Colors.white, textSize: 20, buttonWidth: MediaQuery.of(context).size.width*0.8,),
+                      const SizedBox(height: 20,),
+                       Image.asset("assets/images/continuewithEmail.png"),
+                        const SizedBox(height: 20,),
+                        CustomTextField(textFieldController: name, hintText: "Enter Your Name"),
+                        
                     const SizedBox(height: 20,),
-                   CustomTextField(textFieldController: password, hintText: "Enter Password" , isPass: isPassVisible, textFieldIcon: IconButton(onPressed: (){
+                     CustomTextField(textFieldController: userName, hintText: "Enter Your Username"),
+                    const SizedBox(height: 20,),
+                        CustomTextField(textFieldController: emailAddress, hintText: "Enter Email"),
+                      const SizedBox(height: 20,),
+                     CustomTextField(textFieldController: password, hintText: "Enter Password" , isPass: isPassVisible, textFieldIcon: IconButton(onPressed: (){
+                      setState(() {
+                        isPassVisible =!isPassVisible;
+                      });
+                     }, icon: const Icon(Icons.remove_red_eye_outlined))),
+                   
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                  Checkbox(value: isAgree, onChanged: (value) {
                     setState(() {
-                      isPassVisible =!isPassVisible;
+                      isAgree =!isAgree;
                     });
-                   }, icon: const Icon(Icons.remove_red_eye_outlined))),
-                 
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                Checkbox(value: isAgree, onChanged: (value) {
-                  setState(() {
-                    isAgree =!isAgree;
-                  });
-                },),
-                 SizedBox(
-                  width: MediaQuery.of(context).size.width*0.7,
-                  child: const TextWidget(textMessage: "I agree with the Terms of Service and Privacy policy", textColor: Colors.white, textSize: 15)),
-              ],),
-                  
-                  
-                ],
+                  },),
+                   SizedBox(
+                    width: MediaQuery.of(context).size.width*0.7,
+                    child: const TextWidget(textMessage: "I agree with the Terms of Service and Privacy policy", textColor: Colors.white, textSize: 15)),
+                ],),
+                    
+                    
+                  ],
+                ),
               ),
-            ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Column(
-                       
-                            children: [
-                              InkWell(
-                    onTap: () {
-                      registerUser();
-                    },
-                    child: CustomButtonWidget( bgColor: Colors.black, textMessage: "Create Account", textColor: Colors.white, textSize: 20, buttonWidth: MediaQuery.of(context).size.width*0.5,)),
-                          
-                               const SizedBox(height: 10,),
-                              const TextWidget(textMessage: "Already have an account?", textColor: Colors.white, textSize: 20),
-                              InkWell(
-                              onTap: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginScreen(),));
-                              },
-                              child: const TextWidget(textMessage: "Login", textColor: Colors.black, textSize: 20),),
-                            const SizedBox(height: 10,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Column(
+                         
+                              children: [
+                                InkWell(
+                      onTap: () {
+                        registerUser();
+                      },
+                      child: CustomButtonWidget( bgColor: Colors.black, textMessage: "Create Account", textColor: Colors.white, textSize: 20, buttonWidth: MediaQuery.of(context).size.width*0.5,)),
                             
-                            ],
-                          ),
-                  ),
-                ],
-              ),
-          ],
+                                 const SizedBox(height: 10,),
+                                const TextWidget(textMessage: "Already have an account?", textColor: Colors.white, textSize: 20),
+                                InkWell(
+                                onTap: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginScreen(),));
+                                },
+                                child: const TextWidget(textMessage: "Login", textColor: Colors.black, textSize: 20),),
+                              const SizedBox(height: 10,),
+                              
+                              ],
+                            ),
+                    ),
+                  ],
+                ),
+            ],
+          ),
         ),
       ),
     );
